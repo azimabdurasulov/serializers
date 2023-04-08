70f8d7413b970a8c161882c39014c5c6edad928d
azimabdurasulov <azimjon20050604@gmail.com>
2023-04-08 06:09:00 +0500

    init

--- api/serializers.py
+++ api/serializers.py
@@ -5,6 +5,8 @@ class TodoSerializer(serializers.Serializer):
     task = serializers.CharField(max_length=50)
     description = serializers.CharField(max_length=100)
     completed = serializers.BooleanField(default=False)
+    created_at = serializers.DateTimeField()
+    updated_at = serializers.DateTimeField()
 
     def create(self, validated_data):
         todo = Todo.objects.create(
